f7fe8a683f2179c33ac2b76f1c04299da5d4fd9d
cortesana <a.chdez2020@gmail.com>
2020-05-06 12:46:19 +0200

    Add contentScript.js

---
+++ src/contentScript.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function getTextNodes(node) {
+
+    switch(node.nodeType) {
+        case 1:
+        case 9:
+        case 11:
+            let i = 0;
+            let nodeChildren = node.childNodes;
+            while(nodeChildren[i]) {
+                getTextNodes(nodeChildren[i]);
+                nodeChildren[i] = nodeChildren[i++];
+            }
+            break;
+        case 3:
+            findAndReplace(node);
+    }
+
+}
+
+function findAndReplace(textNode) {
+
+    let textNodeContent = textNode.nodeValue;
+
+    // English
+    textNodeContent = textNodeContent.replace(/\bTungsten\b/g, "Wolfram");
+    textNodeContent = textNodeContent.replace(/\btungsten\b/g, "wolfram");
+    // Spanish
+    textNodeContent = textNodeContent.replace(/\bTungsteno\b/g, "Wolframio");
+    textNodeContent = textNodeContent.replace(/\btungsteno\b/g, "wolframio");
+    textNode.nodeValue = textNodeContent;
+
+}
+
+getTextNodes(document.body);
