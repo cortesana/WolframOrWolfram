edc72f9f632368054acb804455af10b9c139c067
cortesana <a.chdez2020@gmail.com>
2020-05-06 12:57:32 +0200

    Rm 'use strict' contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,5 +1,3 @@
-'use strict';
-
 function getTextNodes(node) {
 
     let tagName = node.tagName ? node.tagName.toLowerCase() : "";
@@ -7,6 +5,7 @@ function getTextNodes(node) {
     if(tagName =='input' || tagName == 'textarea') {
         return;
     }
+    
     switch(node.nodeType) {
         case 1:
         case 9:
@@ -28,14 +27,13 @@ function findAndReplace(textNode) {
 
     let textNodeContent = textNode.nodeValue;
 
-    // English
+    // ENGLISH
     textNodeContent = textNodeContent.replace(/\bTungsten\b/g, "Wolfram");
     textNodeContent = textNodeContent.replace(/\btungsten\b/g, "wolfram");
-    // Spanish
+    // SPANISH
     textNodeContent = textNodeContent.replace(/\bTungsteno\b/g, "Wolframio");
     textNodeContent = textNodeContent.replace(/\btungsteno\b/g, "wolframio");
     textNode.nodeValue = textNodeContent;
-
+    
 }
-
 getTextNodes(document.body);
